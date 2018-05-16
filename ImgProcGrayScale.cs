75d15bc918c58f093f96e858a75be797f09854b0
8gatsu <8gatsu+github@gmail.com>
2018-05-17 06:37:56 +0900

    シンボリックリンクから実体に修正

--- ImgProcGrayScale.cs
+++ ImgProcGrayScale.cs
@@ -1,1 +1,156 @@
-C:/Users/Public/Documents/Unity Projects/New Unity Project 2/Assets/Scripts/ImgProcGrayScale.cs
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImgProcGrayScale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToGray(Color32[] colors, int width, int height) {
+        Color32 rc = new Color32();
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color32 c = colors[x + y * width];
+                byte gray = (byte)(0.1f * c.r + 0.7f * c.g + 0.2f * c.b);
+                //rc.r = rc.g = rc.b = gray;
+                rc.r = rc.g = rc.b = gray;
+                rc.a = 255;
+                colors[x + y * width] = rc;
+            }
+        }
+
+    }
+
+    float[,] sobelX = new float[,] {
+        { 1, 0, -1},
+        { 2, 0, -2},
+        { 1, 0, -1}
+    };
+    float[,] sobelY = new float[,] {
+        {  1,  2,  1},
+        {  0,  0,  0},
+        { -1, -2, -1}
+    };
+    float[,] gaussian = new float[,] {
+        { 1,1,1 },
+        { 1,1,1 },
+        { 1,1,1 }
+    };
+
+
+    public static void Filtering
+        (
+        byte[] src, byte[] dst, int width, int height,
+        float[,] filter, int filter_numerator = 1, int filter_denominator = 1,
+        bool absolute_value = true,
+        bool normalize = false, float normalize_param= -1.0f
+        )
+    {
+        int[] result = new int[dst.Length];
+        for (int i = 0; i < result.Length; i++) { result[i] = 0; }
+
+        int fsizeX = filter.GetLength(1);
+        int fsizeY = filter.GetLength(0);
+        int marginX = fsizeX / 2;
+        int marginY = fsizeY / 2;
+
+        for (int y = marginY; y < height - marginY -1; y++)
+        {
+            for (int x = marginX; x < width - marginX - 1; x++)
+            {
+                for(int yi = 0; yi < fsizeY; yi++)
+                {
+                    for (int xi = 0; xi < fsizeX; xi++) {
+                        result[y * width + x] += (int)(filter[yi,xi] * src[(y+yi-marginY)*width+x+xi-marginX]);
+                    }
+                }
+                if (filter_numerator != 1 || filter_denominator != 1) result[y * width + x] = result[y * width + x] * filter_numerator / filter_denominator;
+                if (absolute_value && result[y * width + x] < 0) result[y * width + x] = -result[y * width + x];
+            }
+        }
+
+        if (normalize)
+        {
+            int max = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] > max) max = result[i];
+            }
+            if (normalize_param < 0) {
+                int total = 0;
+                for (int i = 0; i < result.Length; i++)
+                {
+                    total += result[i];
+                }
+                int avg = total / (width * height);
+                if (avg == 0) avg = 1;
+                if (max >= avg * normalize_param) max = (int)(avg * normalize_param);
+            }
+            for (int i = 0; i < result.Length; i++) { dst[i] = (byte)(result[i] * 255 / max); }
+        }
+        else
+        {
+            for (int i = 0; i < result.Length; i++) { dst[i] = (byte)result[i]; }
+        }
+
+    }
+
+
+    void ByteToColor(byte[] src, Color32[] dst) {
+        int length = src.Length >= dst.Length ? dst.Length : src.Length;
+        if (length == 0) return;
+        for (int i = 0; i < length; i++) {
+            dst[i].r = dst[i].g = dst[i].b = src[i];
+        }
+    }
+
+    void ColorToByte(Color32[] src, byte[] dst)
+    {
+        int length = src.Length >= dst.Length ? dst.Length : src.Length;
+        if (length == 0) return;
+        for (int i = 0; i < length; i++)
+        {
+            dst[i]= src[i].r;
+        }
+
+    }
+
+    public void Sample1(Color32[] colors, int width, int height) {
+        byte[] tmp = new byte[colors.Length];
+
+        ColorToByte(colors, tmp);
+        Filtering(tmp, tmp, width, height, gaussian, 1, 9);
+        Filtering(tmp, tmp, width, height, sobelX, 1, 1, true, true, 4.0f);
+        ByteToColor(tmp, colors);
+    }
+
+    public int threshold = 128;
+    public void Threshold(Color32[] colors, int width, int height)
+    {
+        Color32 rc = new Color32();
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color32 c = colors[x + y * width];
+                byte gray = (byte)( (c.r> threshold) ? 255:0);
+                rc.r = rc.g = rc.b = gray;
+                rc.a = 255;
+                colors[x + y * width] = rc;
+            }
+        }
+    }
+
+}
--- WebCamController.cs
+++ WebCamController.cs
@@ -1,1 +1,121 @@
-C:/Users/Public/Documents/Unity Projects/New Unity Project 2/Assets/Scripts/WebCamController.cs
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamController : MonoBehaviour {
+    public int targetCamID = 0;
+    public int width = 640;
+    public int height = 480;
+    public int fps = 30;
+    public bool autoRun = false;
+
+    public Texture2D outputTexture;     // 出力結果 RawImageに張り付ける
+
+    // 取得した画像に適用する画像処理メソッド
+    // 引数は Color32[] ピクセルデータ
+    //        int       width
+    //        int       height
+    [System.Serializable]
+    public class ImgProcEvent : UnityEngine.Events.UnityEvent<Color32[], int, int> { };
+
+    [SerializeField]
+    ImgProcEvent imageProc = new ImgProcEvent();
+
+    static WebCamTexture[] webcamTexture = null;
+    static Texture2D[] outputTextureArray = null;
+    Color32[] colors = null;
+
+    // Use this for initialization
+    void Start () {
+        if (autoRun) { On(); }
+    }
+
+    public void camChange() {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        if (devices.Length-1 > targetCamID && targetCamID>=0) { targetCamID++; }
+        else { targetCamID = 0; }
+        On(targetCamID);
+    }
+
+    public void On(int camID) {
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length <= camID) return;
+
+
+        if (webcamTexture == null) webcamTexture = new WebCamTexture[devices.Length];
+        if (outputTextureArray == null) outputTextureArray = new Texture2D[devices.Length];
+
+        if (webcamTexture[camID] == null)
+        {
+            webcamTexture[camID] = new WebCamTexture(devices[camID].name, this.width, this.height, this.fps);
+        }
+        else
+        {
+            if (webcamTexture[camID].width != this.width || webcamTexture[camID].height != this.height || (int)webcamTexture[camID].requestedFPS != this.fps)
+            {
+                if(webcamTexture[camID].isPlaying)webcamTexture[camID].Stop();
+                webcamTexture[camID] = new WebCamTexture(devices[camID].name, this.width, this.height, this.fps);
+            }
+        }
+
+        if (!webcamTexture[camID].isPlaying) {
+            webcamTexture[camID].Play();
+        }
+
+
+        colors = new Color32[webcamTexture[camID].width * webcamTexture[camID].height];
+        outputTexture = outputTextureArray[camID] = new Texture2D(webcamTexture[camID].width, webcamTexture[camID].height, TextureFormat.RGBA32, false);
+        targetCamID = camID;
+
+        if (GetComponent<Renderer>() != null)
+        {
+            GetComponent<Renderer>().material.mainTexture = outputTexture;
+        }
+        else if (GetComponent<RawImage>() != null)
+        {
+            GetComponent<RawImage>().texture = outputTexture;
+        }
+    }
+
+    public void On() {
+        On(targetCamID);
+    }
+
+    public void Off(int camID)
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length <= camID) return;
+        if (webcamTexture[camID] == null)
+        {
+            return;
+        }
+        else {
+            webcamTexture[camID].Stop();
+            webcamTexture[camID] = null;
+        }
+    }
+
+    public void Off() {
+        Off(targetCamID);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+ 
+        if (colors != null)
+        {
+            webcamTexture[targetCamID].GetPixels32(colors);
+
+            imageProc.Invoke(colors, width, height);
+
+            outputTexture.SetPixels32(colors);
+            outputTexture.Apply();
+        }
+
+    }
+
+}
